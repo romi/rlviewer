ccd1767c4663e53ff86b92ad1823ff44b9ece92e
Doug Boari <doug@tastymushroom.com>
2022-06-03 07:05:05 +0200

    Replaced old loader

--- src/objloader.hpp
+++ src/objloader.hpp
@@ -3,6 +3,13 @@
 
 bool loadOBJ(
         const char * path,
+        std::vector<glm::vec3> & out_vertices,
+        std::vector<glm::vec2> & out_uvs,
+        std::vector<glm::vec3> & out_normals
+);
+
+bool loadOBJ_new(
+        const char * path,
         std::vector<glm::vec4> & vertices,
         std::vector<glm::vec3> & normals,
         std::vector<GLushort> & elements
